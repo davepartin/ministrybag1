332d5548c533a7e538e30f420d2f64bf49629c3e
Partin <dpartin@LM25DPARTIN.local>
2026-07-16 07:44:38 -0500

    Add public Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 import { sites } from './build/sites-vite-plugin';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [react(), sites(), cloudflare()],
 });
